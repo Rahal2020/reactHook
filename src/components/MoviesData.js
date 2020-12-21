f8d740e3233c9806c144f2821a0481f1a5c58f89
Rahal2020 <rahal.hasnia@gmail.com>
2020-12-21 09:58:48 +0100

    first commit

---
+++ src/components/MoviesData.js
@@ -0,0 +1,45 @@
+
+const MoviesData = [
+    {
+        id: Math.random(),
+        name:'Baby Boss',
+        image:
+        'https://fr.web.img4.acsta.net/pictures/17/01/19/10/11/597368.jpg',
+        date: '(2020)',
+        type: 'Comédie',
+        rating: '4',
+        description: 'A new baby is arrival impacts a family, told from the point of view of a delightfully unreliable narrator, a wildly imaginative 7yearold named Tim.',
+    },
+    {
+        id: Math.random(),
+        image:
+        'https://s3-ap-northeast-1.amazonaws.com/peatix-files/event/1706512/cover-xO6vFdIJyW8xviCWWUr5h25zIaz9Owmf.jpeg',
+        name:'the Sponge Bob',
+        date: '(2020)',
+        type: 'Comédie || Familial',
+        rating: 3,
+        description: 'Suite à l’escargotnapping de Gary, son compagnon de toujours, Bob entraîne Patrick dans une folle aventure vers la Cité Perdue d’Atlantic City afin de le retrouver',  
+    },
+
+    {
+        id: Math.random(),
+        image:
+        'https://i.pinimg.com/564x/3f/6c/de/3f6cde9d0b5e25ab8cf26d645f7cdc4a.jpg',
+        name:'the Good Criminal',
+        date: '(2010)',
+        type: 'Action || Familial',
+        rating: 5,
+        description:'Un légendaire voleur de banque décide de se ranger et passe une entente, contre son immunité, avec le FBI qui n’a jamais réussi à lui mettre la main dessus.',
+    },
+    {
+        id: Math.random(),
+        image:
+        'https://www.cinetrafic.fr/images/affiches/medium/aff_4526020200507154732.jpg', 
+        name:'Dangerous Lies',
+        date: '(2015)',
+        type: 'Action',
+        rating: 2,
+        description: 'When a wealthy elderly man dies and unexpectedly leaves his estate to his new caregiver, she s drawn into a web of deception and murder. If she is going to survive, she will have to question everyone s motives. Even the people she loves.', 
+    },
+]
+export default MoviesData
